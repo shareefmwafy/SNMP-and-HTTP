bdaedcadc2a608e63c1af17a24aa7c13540a02f8
shareef <sh.mwafy1@gmail.com>
2024-03-24 03:08:27 +0200

    Add Main Page

---
+++ mainPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="buttons">
+        <div>
+            <a href="./Pages/Page1.php">System Group </a>
+            <a href="./Pages/Page2.php">TCP Group</a>
+        </div>
+        <div>
+            <a href="./Pages/Page3.php">ARP Group </a>
+            <a href="./Pages/Page4.php">SNMP group</a>
+        </div>
+    </div>
+    
+</body>
+</html>
