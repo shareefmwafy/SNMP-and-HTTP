ad4a3fc53a6d9cba0a03496488d9ba1abca7c250
shareef <sh.mwafy1@gmail.com>
2024-03-24 02:50:02 +0200

    return Pages

---
+++ Pages/Page2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+</style>
+</head>
+<body>
+
+<table id="table">
+    <thead>
+        <tr>
+            <th>Connection State</th>
+            <th>Local Address</th>
+            <th>Local Port</th>
+            <th>Remote Address</th>
+            <th>Remote Port</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
+<script src="../JS/Data2.js"></script>
+
+</body>
+</html>
