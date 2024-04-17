c9f97c2c005fa190fde2b073d396dc757ede14d1
ayhamdw <s12029113@stu.najah.edu>
2024-04-17 13:17:18 +0200

    Small Edit

--- Pages/Page2.php
+++ Pages/Page2.php
@@ -4,33 +4,28 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
-   table {
-    margin-top: 150px;
-    border-collapse: collapse;
-    width: 80%;
-    margin: 0 auto; /* Add this line to center the table horizontally */
-}
+    table {
+        border-collapse: collapse;
+        width: 80%;
 
-th, td {
-    border: 1px solid #dddddd;
-    text-align: center;
-    padding: 8px;
-}
-
-th {
-    background-color: #f2f2f2;
-    color: black;
-}
-
-body {
-    font-family: Arial, sans-serif;
-    background: url("../Images/3.jpg") center repeat;
-    height: 700vh;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    color: white;
+    }
+    th, td {
+        border: 1px solid #dddddd;
+        text-align: center;
+        padding: 8px;
+    }
+    th {
+        background-color: #f2f2f2;
+        color:black;
+    }
+    body {
+        font-family: Arial, sans-serif;
+        background: url("../Images/3.jpg") center/cover no-repeat; 
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        color:white;
 }
     
 </style>
